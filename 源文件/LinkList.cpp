3e41aa08eedaae1f460c190b4c6a52e42447b25d
Happyu <151423674+ForselfChanaged@users.noreply.github.com>
2023-12-29 13:22:39 +0800

    Add files via upload

--- 源文件/LinkList.cpp
+++ 源文件/LinkList.cpp
@@ -1,5 +1,10 @@
 #include "LinkList.h"
 #include<stdio.h>
+LinkList::LinkList()
+{
+	head = new ListNode(0, nullptr);
+	size = 0;
+}
 /// <summary>
 /// 获取指定索引值，若索引无效返回-1
 /// </summary>
@@ -8,7 +13,7 @@
 int LinkList::get(int index)
 {
 	if (index < 0) return -1;
-	ListNode* cur = head;
+	ListNode* cur = head->next;
 	while (index--)
 	{
 		if (cur->next == NULL) return-1;
@@ -19,8 +24,10 @@ int LinkList::get(int index)
 
 void LinkList::addAtHead(int val)
 {
-	ListNode* newHead = new ListNode(val, head);
-	head = newHead;
+	ListNode* newHead = new ListNode(val);
+	newHead->next = head->next;
+	head->next = newHead;
+	size++;
 }
 void LinkList::addAtTail(int val)
 {
@@ -31,26 +38,52 @@ void LinkList::addAtTail(int val)
 	}
 	ListNode* newTail = new ListNode(val, nullptr);
 	tail->next = newTail;
+	size++;
 }
 void LinkList::addAtIndex(int index, int val)
 {
-	if (index <=0) {
+	if (index < 0) {
 		addAtHead(val);
+		return;
+	}
+	if (index == size)
+	{
+		addAtTail(val);
+		return;
+	}
+	if (index > size)
+	{
+		printf("非法索引");
 		return;
 	}
 	ListNode* node = new ListNode(val);
 	ListNode* cur = head;
-	while (--index)
+	while (index--)
 	{
 		cur = cur->next;
 	}
 	node->next = cur->next;
 	cur->next = node;
+	size++;
+}
+void LinkList::delteAtIndex(int index)
+{
+	if (index >= size || index < 0)return;
+	ListNode* cur = head;
+	while (index--)
+	{
+		cur = cur->next;
+	}
+	ListNode* tmp = cur->next;
+	cur->next = cur->next->next;
+	delete tmp;
+	tmp = nullptr;
+	size--;
+	return;
 }
 void LinkList::DeleteVal(int val)
 {
-	ListNode* vrNode = new ListNode(0, head);
-	ListNode* cur = vrNode;
+	ListNode* cur = head;
 	while (cur->next != NULL)
 	{
 		if (cur->next->val == val)
@@ -58,22 +91,27 @@ void LinkList::DeleteVal(int val)
 			ListNode* tmp = cur->next;
 			cur->next = cur->next->next;
 			delete tmp;
+			size--;
 		}
 		else {
 			cur = cur->next;
 		}
 	}
-	head = vrNode->next;
-	delete vrNode;
 }
 void LinkList::Print()
 {
 	ListNode* cur = head;
-	while (cur != NULL)
+	while (cur->next != NULL)
 	{
-		printf(" %d ", cur->val);
+		printf(" %d ", cur->next->val);
 		cur = cur->next;
 
 	}
 	printf("\n");
+}
+
+void LinkList::reverseLink(LinkList list)
+{
+	
+
 }
